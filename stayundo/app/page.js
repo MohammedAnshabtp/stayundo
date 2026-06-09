d0e3cc9c6bb4a738778a42b28aac53c1a7ddd4f2
Rincy Pradeep <rincypradeep15@gmail.com>
2026-06-09 13:07:49 +0530

    configuration

---
+++ stayundo/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Stayundo
+      </h1>
+      <p className="font-bold text-blue-400">
+        Find Your Stay And Essentials
+      </p>
+    </main>
+  );
+}
